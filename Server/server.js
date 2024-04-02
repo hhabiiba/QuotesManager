2a10f944803a7f1a0590dc809f23700ec3d6b28e
hhabiiba <hbmdhb.2003@gmail.com>
2024-04-02 14:03:51 -0700

    Deployed

--- Server/server.js
+++ Server/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
 // Routes
 app.use('/api/quotes', quoteRouter);
 app.use('/api/quoteLists', quoteListsRouter);
